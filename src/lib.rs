aa5813d975bb4a12a979555ad2f8410c9f41dcc1
Henrique Matheus da Silva Lima <hmslima1992@gmail.com>
2022-07-29 19:18:20 -0300

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,42 @@
 use rand::Rng;
 
+#[derive(Debug)]
 pub struct Sound {
     optional: bool,
     is_it_first_optional: bool,
     locked_to_previous_optional: bool,
     sounds: Vec<char>,
+}
+
+impl Sound {
+    fn clone (&self) -> Sound {
+        let new_sound = Sound {
+            optional: self.optional.clone(),
+            is_it_first_optional: self.is_it_first_optional.clone(),
+            locked_to_previous_optional: self.locked_to_previous_optional.clone(),
+            sounds: self.sounds.clone(),
+        };
+
+        new_sound
+    }
+}
+
+fn are_the_patterns_the_same (one: &Vec<Sound>, other: &Vec<Sound>) -> bool {
+
+    let mut counter: usize = 0;
+
+    if one.len() != other.len() {
+        return false;
+    }
+    else {
+        for _n in one {
+            if one[counter].sounds != other[counter].sounds {
+                return false;
+            }
+            counter += 1;
+        }
+    }
+    return true;
 }
 
 fn parse_chosen_sound_characters(raw_elements: &String) -> Vec<String> {
@@ -115,41 +147,92 @@ pub fn parse_pattern(raw_pattern: &String, raw_set_of_strings: &String) -> Vec<S
     pattern
 }
 
-pub fn generate_word(pattern: &Vec<Sound>) -> String {
+pub fn parse_antipatterns(raw_antipatterns: &String, raw_set_of_strings: &String) -> Vec<Vec<Sound>> {
+
+    let mut anti_patterns = Vec::<Vec::<Sound>>::new();
+
+    let mut temp_string = String::new();
+
+    for character in raw_antipatterns.chars() {
+
+        if character == ' ' {
+            continue;
+        }
+        else if character == ';' || character == ',' || character == '.' || character == '\n' || character == '\r' {
+            anti_patterns.push(parse_pattern(&temp_string, &raw_set_of_strings));
+            temp_string.clear();
+        }
+        else {
+            temp_string.push(character);
+        }
+    }
+
+    anti_patterns
+}
+
+pub fn generate_word(pattern: &Vec<Sound>, antipatterns: &Vec<Vec<Sound>>) -> String {
+
+    if pattern.len() == 0 {
+        return String::from("");
+    }
 
     let mut rng = rand::thread_rng();
+
+    let mut actual_pattern = Vec::<Sound>::new();
 
     let mut new_word = String::new();
 
     let mut previous_optional_character_was_used: bool = false;
 
-    for character in pattern {
-        let use_character: bool = rng.gen_bool(0.5);
-        let number_of_characters = character.sounds.len();
-        let which_character = rng.gen_range(0..number_of_characters);
+    let mut still_is_necessary_find_a_result: bool = true;
 
-        if !character.optional {
-            new_word.push(character.sounds[which_character]);
-            previous_optional_character_was_used = false;
-        }
-        else {
-            if character.is_it_first_optional {
-                if use_character {
-                    new_word.push(character.sounds[which_character]);
-                    previous_optional_character_was_used = true;
-                }
-            }
-            else if character.locked_to_previous_optional && previous_optional_character_was_used {
+    while still_is_necessary_find_a_result {
+        for character in pattern {
+            let use_character: bool = rng.gen_bool(0.5);
+            let number_of_characters = character.sounds.len();
+            let which_character = rng.gen_range(0..number_of_characters);
+    
+            if !character.optional {
                 new_word.push(character.sounds[which_character]);
-            }
-            else if character.locked_to_previous_optional && !previous_optional_character_was_used {
-                continue;
+                previous_optional_character_was_used = false;
+    
+                actual_pattern.push(character.clone());
             }
             else {
-                if use_character {
-                    new_word.push(character.sounds[which_character]);
+                if character.is_it_first_optional {
+                    if use_character {
+                        new_word.push(character.sounds[which_character]);
+                        previous_optional_character_was_used = true;
+    
+                        actual_pattern.push(character.clone());
+                    }
                 }
-                previous_optional_character_was_used = false;
+                else if character.locked_to_previous_optional && previous_optional_character_was_used {
+                    new_word.push(character.sounds[which_character]);
+    
+                    actual_pattern.push(character.clone());
+                }
+                else if character.locked_to_previous_optional && !previous_optional_character_was_used {
+                    continue;
+                }
+                else {
+                    if use_character {
+                        new_word.push(character.sounds[which_character]);
+    
+                        actual_pattern.push(character.clone());
+                    }
+                    previous_optional_character_was_used = false;
+                }
+            }
+        }
+
+        still_is_necessary_find_a_result = false;
+    
+        for antipattern in antipatterns {
+            if are_the_patterns_the_same(&actual_pattern, antipattern) {
+                actual_pattern.clear();
+                still_is_necessary_find_a_result = true;
+                new_word.clear();
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,23 @@ use langmater::*;
 fn main() {
 
     let mut raw_pattern = String::new();
+    let mut raw_antipatterns = String::new();
 
     let stdin = io::stdin();
 
     println!("Insert the pattern:");
-
     stdin.read_line(&mut raw_pattern).expect("Couldn't read from stdin");
+
+    println!("Insert the antipatterns:");
+    stdin.read_line(&mut raw_antipatterns).expect("Couldn't read from stdin");
 
     let raw_strings = String::from("C: bcdfghjklmnpqrstvxz; V: aeiouäöüé; S: ŭĭ;");
 
     let pattern = parse_pattern(&raw_pattern, &raw_strings);
+    let antipatterns = parse_antipatterns(&raw_antipatterns, &raw_strings);
 
-
-    for _n in 1..10 {
-        let new_word = String::from(generate_word(&pattern));
+    for _n in 1..30 {
+        let new_word = String::from(generate_word(&pattern, &antipatterns));
 
         println!("> {}", new_word);
     }
